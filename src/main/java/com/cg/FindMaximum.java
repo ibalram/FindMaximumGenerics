82af5b7818768ba5bec2888873bac299cf7b99e7
ibalram <balramsinghrathore3@gmail.com>
2020-10-09 02:47:50 +0530

    Added: Extend max method to take more than 3 params

--- src/main/java/com/cg/FindMaximum.java
+++ src/main/java/com/cg/FindMaximum.java
@@ -1,24 +1,23 @@
 package com.cg;
 
-public class FindMaximum <T extends Comparable>{
-	T a,b,c;
-	
+public class FindMaximum<T extends Comparable> {
+	T a, b, c;
+
 	public FindMaximum(T a, T b, T c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
-	
+
 	public T maximumTest() {
-		return maximumTest(a,b,c);
+		return maximumTest(a, b, c);
 	}
 
-	public static <T extends Comparable> T maximumTest(T a, T b, T c) {
+	public static <T extends Comparable> T maximumTest(T a, T... b) {
 		T max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
+		for (T elem : b)
+			if (elem.compareTo(max) > 0)
+				max = elem;
 		return max;
 	}
 
@@ -29,9 +28,9 @@ public class FindMaximum <T extends Comparable>{
 		System.out.println(maximumTest(aInteger, bInteger, cInteger));
 		System.out.println(maximumTest(aDouble, bDouble, cDouble));
 		System.out.println(maximumTest(aString, bString, cString));
-		
-		System.out.println(new FindMaximum<Integer>(aInteger,bInteger,cInteger).maximumTest());
-		System.out.println(new FindMaximum<Double>(aDouble,bDouble,cDouble).maximumTest());
-		System.out.println(new FindMaximum<String>(aString,bString,cString).maximumTest());
+
+		System.out.println(new FindMaximum<Integer>(aInteger, bInteger, cInteger).maximumTest());
+		System.out.println(new FindMaximum<Double>(aDouble, bDouble, cDouble).maximumTest());
+		System.out.println(new FindMaximum<String>(aString, bString, cString).maximumTest());
 	}
 }
