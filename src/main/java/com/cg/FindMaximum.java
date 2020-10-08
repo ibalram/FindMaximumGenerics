d1982218a141f9efaab3e38a141e1fa90b17b573
ibalram <balramsinghrathore3@gmail.com>
2020-10-09 02:25:08 +0530

    Refactored: One Generic method for all Integer, Double and Strings

--- src/main/java/com/cg/FindMaximum.java
+++ src/main/java/com/cg/FindMaximum.java
@@ -2,26 +2,8 @@ package com.cg;
 
 public class FindMaximum {
 
-	public static Integer maximumTest(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public static Double maximumTest(Double a, Double b, Double c) {
-		Double max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-	
-	public static String maximumTest(String a, String b, String c) {
-		String max = a;
+	public static <T extends Comparable> T maximumTest(T a, T b, T c) {
+		T max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
