63a75fb04fea99911eaadf4c38b7dde2c22b364d
ibalram <balramsinghrathore3@gmail.com>
2020-10-09 02:58:36 +0530

    Added: Max method print the maximum using std out

--- src/main/java/com/cg/FindMaximum.java
+++ src/main/java/com/cg/FindMaximum.java
@@ -20,6 +20,20 @@ public class FindMaximum<T extends Comparable> {
 				max = elem;
 		return max;
 	}
+	
+	public static <T extends Comparable> T maximumTest(T a, T b, T c) {
+		T max = a;
+		if (b.compareTo(max)>0)
+			max = b;
+		if (c.compareTo(max)>0)
+			max = c;
+		printMax(a,b,c,max);
+		return max;
+	}
+	
+	public static <T> void printMax(T a, T b, T c, T max) {
+		System.out.println(String.format("Maximum of %s, %s and %s is: %s", a,b,c,max));
+	}
 
 	public static void main(String[] args) {
 		Integer aInteger = 5, bInteger = 10, cInteger = 15;
@@ -29,8 +43,8 @@ public class FindMaximum<T extends Comparable> {
 		System.out.println(maximumTest(aDouble, bDouble, cDouble));
 		System.out.println(maximumTest(aString, bString, cString));
 
-		System.out.println(new FindMaximum<Integer>(aInteger, bInteger, cInteger).maximumTest());
-		System.out.println(new FindMaximum<Double>(aDouble, bDouble, cDouble).maximumTest());
-		System.out.println(new FindMaximum<String>(aString, bString, cString).maximumTest());
+		new FindMaximum<Integer>(aInteger, bInteger, cInteger).maximumTest();
+		new FindMaximum<Double>(aDouble, bDouble, cDouble).maximumTest();
+		new FindMaximum<String>(aString, bString, cString).maximumTest();
 	}
 }
